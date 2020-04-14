426d8b5138646e1602e85203c052a8a094bd6791
Tom Dane <tdane@tesla.com>
2020-04-13 23:49:40 -0700

    Add stringsplosion solution

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace algorithmsDataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Stringsplosion.Run("Code"));
+            Console.WriteLine(Stringsplosion.Run("x"));
+            Console.WriteLine(Stringsplosion.Run("ab"));
+            Console.WriteLine(Stringsplosion.Run("Bad"));
+        }
+    }
+}
---
+++ Stringsplosion.cs
@@ -0,0 +1,17 @@
+﻿namespace algorithmsDataStructures
+{
+    class Stringsplosion
+    {
+        public static string Run(string input)
+        {
+            var result = "";
+
+            for(var i = 0; i < input.Length; i++)
+            {
+                result += input.Substring(0, i + 1);
+            }
+
+            return result;
+        }
+    }
+}
